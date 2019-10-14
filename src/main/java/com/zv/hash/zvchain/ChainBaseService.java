f8250a47db973982fbe2511d9aa59c5ec8bc1886
longtao <ooonesway@gmail.com>
2019-10-14 16:12:37 +0800

    update chain service

--- src/main/java/com/zv/hash/zvchain/ChainBaseService.java
+++ src/main/java/com/zv/hash/zvchain/ChainBaseService.java
@@ -3,7 +3,7 @@ package com.zv.hash.zvchain;
 import com.zv.hash.sign.SignModel;
 import com.zv.hash.sign.SignUtil;
 import com.zv.hash.zvchain.api.ZvApi;
-import com.zv.hash.zvchain.model.MinerInfo;
+import com.zv.hash.zvchain.model.*;
 import retrofit2.Call;
 
 import java.io.IOException;
@@ -21,49 +21,41 @@ public class ChainBaseService {
     static ZvApi zvApi;
 
     public Long getBlockHeight() throws IOException {
-        com.zv.hash.zvchain.ZvRequest zvRequest = new com.zv.hash.zvchain.ZvRequest();
+        ZvRequest zvRequest = new ZvRequest();
         zvRequest.setMethod("Gzv_blockHeight");
-        Call<com.zv.hash.zvchain.Response<Long>> response = zvApi.requestLong(zvRequest);
+        Call<Response<Long>> response = zvApi.requestLong(zvRequest);
         return response.execute().body().getResult();
     }
 
     public Long getNonce(String address) throws IOException {
-        com.zv.hash.zvchain.ZvRequest zvRequest = new com.zv.hash.zvchain.ZvRequest();
+        ZvRequest zvRequest = new ZvRequest();
         zvRequest.setMethod("Gzv_nonce");
         zvRequest.getParams().add(address);
-        Call<com.zv.hash.zvchain.Response<Long>> response = zvApi.requestLong(zvRequest);
+        Call<Response<Long>> response = zvApi.requestLong(zvRequest);
         return response.execute().body().getResult();
     }
 
-    public com.zv.hash.zvchain.model.BlockDetail getBlockDetailByHeight(long height) throws IOException {
-        com.zv.hash.zvchain.ZvRequest zvRequest = new com.zv.hash.zvchain.ZvRequest();
-        zvRequest.setMethod("Dev_blockDetailByHeight");
-        zvRequest.getParams().add(height);
-        Call<com.zv.hash.zvchain.Response<com.zv.hash.zvchain.model.BlockDetail>> response = zvApi.requestBlockDetail(zvRequest);
-        return response.execute().body().getResult();
-    }
-
-    public com.zv.hash.zvchain.model.BlockDetail getBlockDetailByHash(String hash) throws IOException {
-        com.zv.hash.zvchain.ZvRequest zvRequest = new com.zv.hash.zvchain.ZvRequest();
-        zvRequest.setMethod("Dev_blockDetail");
+    public BlockHeader getBlockByHash(String hash) throws IOException {
+        ZvRequest zvRequest = new ZvRequest();
+        zvRequest.setMethod("Gzv_getBlockByHash");
         zvRequest.getParams().add(hash);
-        Call<com.zv.hash.zvchain.Response<com.zv.hash.zvchain.model.BlockDetail>> response = zvApi.requestBlockDetail(zvRequest);
+        Call<Response<BlockHeader>> response = zvApi.requestBlockDetail(zvRequest);
         return response.execute().body().getResult();
     }
 
-    public com.zv.hash.zvchain.model.BlockHeader getBlockHeaderByHeight(long height) throws IOException {
-        com.zv.hash.zvchain.ZvRequest zvRequest = new com.zv.hash.zvchain.ZvRequest();
+    public BlockHeader getBlockByHeight(long height) throws IOException {
+        ZvRequest zvRequest = new ZvRequest();
         zvRequest.setMethod("Gzv_getBlockByHeight");
         zvRequest.getParams().add(height);
-        Call<com.zv.hash.zvchain.Response<com.zv.hash.zvchain.model.BlockHeader>> response = zvApi.requestBlockHeader(zvRequest);
+        Call<Response<BlockHeader>> response = zvApi.requestBlockHeader(zvRequest);
         return response.execute().body().getResult();
     }
 
-    private String sendTx(com.zv.hash.zvchain.model.TxSend txSend) throws IOException {
-        com.zv.hash.zvchain.ZvRequest zvRequest = new com.zv.hash.zvchain.ZvRequest();
+    private String sendTx(TxSend txSend) throws IOException {
+        ZvRequest zvRequest = new ZvRequest();
         zvRequest.setMethod("Gzv_tx");
         zvRequest.getParams().add(txSend.marshal());
-        Call<com.zv.hash.zvchain.Response<String>> response = zvApi.requestTx(zvRequest);
+        Call<Response<String>> response = zvApi.requestTx(zvRequest);
         return response.execute().body().getResult();
     }
 
@@ -83,7 +75,7 @@ public class ChainBaseService {
         signModel.setNonce(BigInteger.valueOf(nonce));
         signModel.setValue(signModel.getValue().multiply(BigInteger.TEN.pow(9)));
         String signResult = SignUtil.sign(privateKey, signModel);
-        com.zv.hash.zvchain.model.TxSend txSend = com.zv.hash.zvchain.model.TxSend.builder()
+        TxSend txSend = TxSend.builder()
                 .sign(signResult)
                 .source(zvcAddress)
                 .target(signModel.getTarget())
@@ -98,45 +90,28 @@ public class ChainBaseService {
         return sendTx(txSend);
     }
 
-    public com.zv.hash.zvchain.model.TxReceive getReceipt(String hash) throws IOException {
-        com.zv.hash.zvchain.ZvRequest zvRequest = new com.zv.hash.zvchain.ZvRequest();
+    public TxReceive getReceipt(String hash) throws IOException {
+        ZvRequest zvRequest = new ZvRequest();
         zvRequest.setMethod("Gzv_txReceipt");
         zvRequest.getParams().add(hash);
-        Call<com.zv.hash.zvchain.Response<com.zv.hash.zvchain.model.TxReceive>> response = zvApi.requestReceipt(zvRequest);
+        Call<Response<TxReceive>> response = zvApi.requestReceipt(zvRequest);
         return response.execute().body().getResult();
     }
 
     public Double getBalance(String address) throws IOException {
-        com.zv.hash.zvchain.ZvRequest zvRequest = new com.zv.hash.zvchain.ZvRequest();
-        zvRequest.setMethod("Gzv_balaSignUtilnce");
+        ZvRequest zvRequest = new ZvRequest();
+        zvRequest.setMethod("Gzv_balance");
         zvRequest.getParams().add(address);
-        Call<com.zv.hash.zvchain.Response<Double>> response = zvApi.requestDouble(zvRequest);
+        Call<Response<Double>> response = zvApi.requestDouble(zvRequest);
         return response.execute().body().getResult();
     }
 
-    public com.zv.hash.zvchain.model.MinerInfo getMinerInfo(String address, String detail) throws IOException {
-        com.zv.hash.zvchain.ZvRequest zvRequest = new com.zv.hash.zvchain.ZvRequest();
+    public MinerInfo getMinerInfo(String address, String detail) throws IOException {
+        ZvRequest zvRequest = new ZvRequest();
         zvRequest.setMethod("Gzv_minerInfo");
         zvRequest.getParams().add(address);
         zvRequest.getParams().add(detail);
-        Call<com.zv.hash.zvchain.Response<MinerInfo>> response = zvApi.getMinerInfo(zvRequest);
-        return response.execute().body().getResult();
-    }
-
-    public com.zv.hash.zvchain.model.BlockHeightReward getBlockHeightRewardInfo(long height) throws IOException {
-        com.zv.hash.zvchain.ZvRequest zvRequest = new com.zv.hash.zvchain.ZvRequest();
-        zvRequest.setMethod("Explorer_explorerBlockReward");
-        zvRequest.getParams().add(height);
-        Call<com.zv.hash.zvchain.Response<com.zv.hash.zvchain.model.BlockHeightReward>> response = zvApi.getBlockHeightRewardInfo(zvRequest);
-        return response.execute().body().getResult();
-    }
-
-    public com.zv.hash.zvchain.model.MinerPoolInfo getMinerPoolInfo(String addr, Long height) throws IOException {
-        com.zv.hash.zvchain.ZvRequest zvRequest = new ZvRequest();
-        zvRequest.setMethod("Gzv_minerPoolInfo");
-        zvRequest.getParams().add(addr);
-        zvRequest.getParams().add(height);
-        Call<Response<com.zv.hash.zvchain.model.MinerPoolInfo>> response = zvApi.getMinerPoolInfo(zvRequest);
+        Call<Response<MinerInfo>> response = zvApi.getMinerInfo(zvRequest);
         return response.execute().body().getResult();
     }
 
--- src/main/java/com/zv/hash/zvchain/api/ZvApi.java
+++ src/main/java/com/zv/hash/zvchain/api/ZvApi.java
@@ -2,14 +2,12 @@ package com.zv.hash.zvchain.api;
 
 import com.zv.hash.zvchain.Response;
 import com.zv.hash.zvchain.ZvRequest;
-import com.zv.hash.zvchain.model.MinerInfo;
+import com.zv.hash.zvchain.model.*;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface ZvApi {
-    @POST("/")
-    Call<Response<Integer>> requestInteger(@Body ZvRequest jsonObject);
 
     @POST("/")
     Call<Response<Long>> requestLong(@Body ZvRequest jsonObject);
@@ -18,23 +16,18 @@ public interface ZvApi {
     Call<Response<Double>> requestDouble(@Body ZvRequest jsonObject);
 
     @POST("/")
-    Call<Response<com.zv.hash.zvchain.model.BlockDetail>> requestBlockDetail(@Body ZvRequest jsonObject);
+    Call<Response<BlockHeader>> requestBlockDetail(@Body ZvRequest jsonObject);
 
     @POST("/")
-    Call<Response<com.zv.hash.zvchain.model.BlockHeader>> requestBlockHeader(@Body ZvRequest jsonObject);
+    Call<Response<BlockHeader>> requestBlockHeader(@Body ZvRequest jsonObject);
 
     @POST("/")
     Call<Response<String>> requestTx(@Body ZvRequest jsonObject);
 
     @POST("/")
-    Call<Response<com.zv.hash.zvchain.model.TxReceive>> requestReceipt(@Body ZvRequest jsonObject);
+    Call<Response<TxReceive>> requestReceipt(@Body ZvRequest jsonObject);
 
     @POST("/")
     Call<Response<MinerInfo>> getMinerInfo(@Body ZvRequest jsonObject);
 
-    @POST("/")
-    Call<Response<com.zv.hash.zvchain.model.BlockHeightReward>> getBlockHeightRewardInfo(@Body ZvRequest jsonObject);
-
-    @POST("/")
-    Call<Response<com.zv.hash.zvchain.model.MinerPoolInfo>> getMinerPoolInfo(@Body ZvRequest jsonObject);
 }
